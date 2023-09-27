6b2cd56a51cf20ff0a54866d9393822268ad9bc0
elemelooon <rissaleijavier@gmail.com>
2023-09-27 13:07:42 +0800

    route

--- src/App.js
+++ src/App.js
@@ -26,16 +26,15 @@ function App() {
       <Route path="/Recipelist" element={<Recipe/>}></Route>
       <Route path="/RandomRecipe" element={<RandomRecipe/>}></Route>
       <Route path="/RandomRestaurant" element={<RandomRestaurant/>}></Route>
-      <Route path="/Addrecipe" element={<Addrecipe/>}></Route>
-      <Route path="/Addingredient" element={<Ingredients/>}></Route>
-      <Route path="/Adddisplay" element={<Adddisplay/>}></Route>
-      <Route path="/Adddisplay" element={<Adddisplay/>}></Route>
-      <Route path="/Addinstruction" element={<Instruction/>}></Route>
-      <Route path="/Addfilter" element={<Addfilter/>}></Route>
-      <Route path="/Addlocation" element={<AddLocation/>}></Route>
-      <Route path="/Addrestaurant" element={<Addrestaurant/>}></Route>
+      <Route path="/9YCKPnFPeixgyLzAddrecipegLlk4jvYcrbGT20" element={<Addrecipe/>}></Route>
+      <Route path="/DwyecEawUeQV2UNAddingredientU4JI6zE5oYgZySI" element={<Ingredients/>}></Route>
+      <Route path="/Y5nbl5gHIGAnnflAdddisplaypO2Tl6OTkF86ZAY" element={<Adddisplay/>}></Route>
+      <Route path="/LMqoY3cd3FPOX0uAddinstructionR6HeUssZSr8I9Ua" element={<Instruction/>}></Route>
+      <Route path="/zNCEg2HVmzcFjSmAddfilteruTb5p2ERR1XQ3Nb" element={<Addfilter/>}></Route>
+      <Route path="/1F62zkfUvvRANjiAddlocationQ6Ih1Vg1824qPNI" element={<AddLocation/>}></Route>
+      <Route path="/5gHPK5qvt5wdUWuAddrestaurantRtxEmhFlHxLqOaF" element={<Addrestaurant/>}></Route>
       <Route path="/Recipelist/recipedetails" element={<Details/>}></Route>
-      <Route path="/AdminLogin" element={<Login/>}></Route>
+      <Route path="/AdminLogin/iXez7pmIiKnfd4FUKVPgXkmeU4loEw" element={<Login/>}></Route>
       
     </Routes>
     </BrowserRouter>
